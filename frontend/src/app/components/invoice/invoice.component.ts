6b9cc9700ff49ebc7fb1a3e32db30a28a5d4b8dc
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-09 16:37:12 -0500

    feat: se crearon todos los componentes de la factura

---
+++ frontend/src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../models/invoice';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { ListItemsComponent } from '../list-items/list-items.component';
+import { RowItemComponent } from '../row-item/row-item.component';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [InvoiceViewComponent,ClientViewComponent,CompanyViewComponent,ListItemsComponent],
+  templateUrl: './invoice.component.html'
+})
+export class InvoiceComponent implements OnInit{
+  invoice!: Invoice
+
+  constructor (private service: InvoiceService){
+
+  }
+  ngOnInit(): void {
+   this.invoice = this.service.getInvoice();
+  }
+
+}
---
+++ frontend/src/app/components/row-item/row-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Item } from '../../models/item';
+
+@Component({
+  selector: ' tr[app-row-item]',
+  standalone: true,
+  imports: [],
+  templateUrl: './row-item.component.html'
+})
+export class RowItemComponent {
+  @Input() items!: Item;
+
+}
