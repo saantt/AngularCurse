6badb842127e7f68ac60c74a0939f1022c3e20a3
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-15 16:12:22 -0500

    Aplicacion completa del carrito de compras

--- frontend/src/app/components/cart-app.component.ts
+++ frontend/src/app/components/cart-app.component.ts
@@ -17,17 +17,54 @@ export class CartAppComponent implements OnInit {
 
   items: CartItem[] = [];
 
+  total: number = 0;
+  showCart : boolean = false;
+
   constructor(private service: ProductService) { }
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
+    this.calculateTotal();
   }
-  onAddCart(product: Product) {
-    this.items = [...this.items, { product: { ...product }, quantity: 1 }]
+  onAddCart(product: Product): void {
+    const hasItem = this.items.find(item => item.product.id === product.id)
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1
+          }
+        }
+        return item;
+      })
+
+    } else {
+
+      this.items = [...this.items, { product: { ...product }, quantity: 1 }]
+    }
+    this.calculateTotal();
+    this.saveSession();
   }
 
-  getTotal(): number {
-    return this.products.reduce((sum, product) => sum + product.price, 0);
+  onDeleteCart(id: number): void {
+    this.items = this.items.filter(item => item.product.id !== id);
+    this.calculateTotal();
+    this.saveSession();
+
+  }
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0)
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
+  }
+
+  openCart(): void {
+    this.showCart = !this.showCart
   }
 
 }
--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
@@ -9,9 +9,18 @@ import { CartItem } from '../../models/cartItem';
 })
 export class CartComponent {
 
-  @Input()items:CartItem[] =[];
+  @Input() items: CartItem[] = [];
+  @Input() total: number=0;
+
+  @Output() idProductEventEmitter = new EventEmitter();
+
+
+
+  onDeleteCart(id: number) {
+    this.idProductEventEmitter.emit(id);
+
+  }
 
 
 
 }
- 
